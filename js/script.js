19b5b0d635637c6e9788d0245125890ec018a410
Moringa School <karanjasam210@gmail.com>
2022-03-21 21:19:26 +0300

    ci: add function and prototype to calculate order

--- js/script.js
+++ js/script.js
@@ -1,40 +1,58 @@
-function calculate(size, crust, toppings, amount, price){
-    this.size = size;
-    this.crust = crust;
-    this.toppings = toppings;
-    this.amount = amount;
-    this.price = price;
-}
-calculate.prototype.cost = function(){ 
-
-    if(this.size === "Large"){
-        var cost = 1200;
-    }else if(this.size === "Medium"){
-        var cost = 900;
-    }else if(this.size ==="Small"){
-        var cost = 700;
-    }
-
-    if(this.crust === "stuffed"){
-        var crustPrice = 200;
-    }else if (this.crust === "gluten-free"){
-        var crustPrice = 150;
-    }else if (this.crust === "crispy"){
-        var crustPrice = 250;
-    }
-
-alert("You have selected + this.pieces ")
-
-calculate.prototype.name =function(){
-    let pizzaName =`${this.amount} ${this.size} ${this.crust} ${'crusted pizza, with'} ${this.toppings} ${"for the topping."}`;
-    console.log(pizzaName);
+function Order(flavour, toppings, size, crust, pieces) {
+  this.flavour = flavour;
+  this.toppings = toppings;
+  this.size = size;
+  this.crust = crust;
+  this.pieces = pieces;
 }
 
+Order.prototype.cost = function () {
+  if (this.size === "large") {
+    let price = 1200;
+  } else if (this.size === "medium") {
+    let price = 900;
+  } else if(this.size === "small"){
+    let price = 700;
+  }
 
-$("#button1").click(function(){
-    alert("Your message has been received. Thank you for reaching out to us")
+  if(this.toppings=== "olives"){
+      let toppingsPrice = 120;
+    
+  }
+  else if(this.toppings === "mushroom"){
+      let toppingsPrice = 150;
+  }
+  else if (this.toppings === "pineapple"){
+      let toppingsPrice = 180;
+  }
+  else if(this.toppings === "tomato"){
+      let toppingsPrice = 160;
+  }
+  else if(this.toppings === "veggie-salad"){
+      let toppingsPrice = 170;
+  }
+ 
+  if(this.crust === "gluten-free"){
+      let crustPrice = 50;
+  }
+  else if (this.crust === "crispy"){
+      let crustPrice = 80;
+  }
+ else if (this.crust === "stuffed"){
+     let crustPrice =  70;
+ }
+ return (price + toppingsPrice + crustPrice )* this.pieces;
+};
+
+$("#order-form").submit(function(event){
+    event.preventDefault();
+    let flavour = $("#flavour").val();
+    let toppings = $("#toppings").val();
+    let size = $("#sizee").val();
+    let crust = $("#crust").val();
+
+   
 })
 
-$("#checkout").click(function(){
-    alert("You have selected" + this.pieces + )
-})
+// price();
+console.log(price)
